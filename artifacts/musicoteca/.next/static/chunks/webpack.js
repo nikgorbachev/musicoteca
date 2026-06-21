792336ee5039748ef5397ee468b93cdfa22d601d
Nikolai Gorbachev <88505773+nikgorbachev@users.noreply.github.com>
2026-06-21 20:12:55 +0000

    fixing inconsistent states, lacking images, ui refinements

--- artifacts/musicoteca/.next/static/chunks/webpack.js
+++ artifacts/musicoteca/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6785467ad0701e86"; }
+/******/ 		__webpack_require__.h = function() { return "a0f16b91d37d6be8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
